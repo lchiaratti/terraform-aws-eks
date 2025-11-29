994dc2cdd84f116d5302c2e7d82b931ff682d873
Lucas Chiaratti <chiaratti@gmail.com>
2025-11-29 16:24:48 -0300

    2 commit

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.23.0"
     }
+  }
+  backend "s3" {
+    bucket = "eks-tfstate-chiaratti"
+    key    = "dev/eks-dev.tfstate"
+    region = "us-east-1"
   }
 }
 
